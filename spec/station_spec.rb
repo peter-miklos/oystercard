5b1a04366cfb27a69fda7f02f543fec62e3b59c3
Matthew Perkins <mfperkins@gmail.com>
2016-08-23 12:53:40 +0100

    Step 13 complete: added station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,22 @@
+require 'station'
+
+describe Station do
+
+  context do 'when initialize'
+
+    before(:each) do
+      station = Station.new("Liverpool Street", 1)
+
+      it 'has a name' do
+        expect(station.name).to eq "Liverpool Street"
+      end
+
+      it 'has a zone' do
+        expect(station.zone).to eq 1
+      end
+
+    end
+
+  end
+
+end
