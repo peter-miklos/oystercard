f3b203f329ba1ecf46898b875f237751a45e6b69
Peter Miklos <petermiklos@Peters-MacBook-Pro.local>
2016-08-26 11:25:46 +0100

    add changes in journey log

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -12,29 +12,48 @@ class JourneyLog
 
   def start(station = nil)
     fare = 0
-    if current_journey
+    if @current_journey
+      @current_journey = current_journey(station)
       fare = @current_journey.fare
+      @journeys << @current_journey
       @current_journey = nil
     end
-    @current_journey = journey(station)
-    @journey_log << current_journey
+    @current_journey = current_journey(station)
     fare
+    # fare = 0
+    # if current_journey
+    #   fare = @current_journey.fare
+    #   @current_journey = nil
+    # end
+    # @current_journey = journey(station)
+    # @journey_log << current_journey
+    # fare
   end
 
   def finish(station)
-    fare = 0
-    if !current_journey
-      @current_journey = journey(nil)
-      @current_journey.finish(station)
-      @journeys << current_journey
+    if !@current_journey
+      @current_journey = current_journey
       fare = @current_journey.fare
+      @journeys << @current_journey
+      @current_journey = nil
     else
-      @current_journey.finish(station)
-      @journey_log.pop
-      @journey_log << current_journey
     end
-    @current_journey = nil
-    fare
+
+
+
+    # fare = 0
+    # if !@current_journey
+    #   @current_journey = journey(nil)
+    #   @current_journey.finish(station)
+    #   @journeys << current_journey
+    #   fare = @current_journey.fare
+    # else
+    #   @current_journey.finish(station)
+    #   @journey_log.pop
+    #   @journey_log << current_journey
+    # end
+    # @current_journey = nil
+    # fare
   end
 
   def journeys
@@ -53,9 +72,9 @@ class JourneyLog
 
   attr_reader :journey_class
 
-  # def current_journey(station)
-  #   current_journey || journey(station)
-  # end
+  def current_journey(station)
+    current_journey || journey(station)
+  end
 
   def journey(station)
     journey_class.new(station)
